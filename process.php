333daccc8365113d906636a4248bbdc5eaaa6701
Yajur Rai <yajur.rai@outlook.com>
2020-11-28 15:35:15 +0545

    moved files to root

--- process.php
+++ process.php
@@ -2,11 +2,11 @@
   use PHPMailer\PHPMailer\PHPMailer;
   use PHPMailer\PHPMailer\Exception;
 
-  require_once "../vendor/autoload.php";
+  require_once "./vendor/autoload.php";
 
-  require '../vendor/phpmailer/phpmailer/src/Exception.php';
-  require '../vendor/phpmailer/phpmailer/src/PHPMailer.php';
-  require '../vendor/phpmailer/phpmailer/src/SMTP.php';
+  require './vendor/phpmailer/phpmailer/src/Exception.php';
+  require './vendor/phpmailer/phpmailer/src/PHPMailer.php';
+  require './vendor/phpmailer/phpmailer/src/SMTP.php';
 
   if(isset($_POST['submit'])) {
     // GET POST DATA
